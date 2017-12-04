167174533285b633e8ae3cc7057b0ca0253055a1
jliberma <jacobliberman@gmail.com>
2017-12-04 10:34:45 -0500

    update environment files

--- scripts/swift_ring.sh
+++ scripts/swift_ring.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # verify swift rings
-swift-ring-builder /etc/swift/account.builder
-swift-ring-builder /etc/swift/container.builder
-swift-ring-builder /etc/swift/object.builder
+sudo swift-ring-builder /etc/swift/account.builder
+sudo swift-ring-builder /etc/swift/container.builder
+sudo swift-ring-builder /etc/swift/object.builder
 swift-recon --md5
 
 # verify swift backend for glance
